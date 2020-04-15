15f5dc3292731ce350e88c58f2faf35734aca568
Joey <joey.alzamora@gmail.com>
2020-04-15 02:36:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,10 +3,5 @@ def introduction(name)
   {name}."
 end
 
-def introduction_with_language(name, language)
-  puts "Hi, my name is #
-  {name}."
-  puts "I am learning to program in #
-  {language}."
-end
+
 
